0b01c326cbc0d877bfd228126a9d01ab2a1325ec
Peeyush Pandey <110827516+Luffy-afk10@users.noreply.github.com>
2024-02-27 21:40:06 +0530

    Update Iris Classification.py

--- Iris Classification.py
+++ Iris Classification.py
@@ -4,29 +4,28 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-# Load Iris dataset
+
 iris = load_iris()
-X = iris.data  # Features
-y = iris.target  # Target variable
+X = iris.data  
+y = iris.target  
 
-# Split dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Feature scaling (optional but recommended)
+X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_tr = scaler.fit_transform(X_tr)
+X_te = scaler.transform(X_te)
 
-# Initialize and train the Random Forest classifier
 rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-rf_classifier.fit(X_train, y_train)
+rf_classifier.fit(X_tr, y_tr)
 
-# Predict on the test set
-y_pred = rf_classifier.predict(X_test)
 
-# Calculate accuracy
-accuracy = accuracy_score(y_test, y_pred)
+y_pred = rf_classifier.predict(X_te)
+
+
+accuracy = accuracy_score(y_te, y_pred)
 print("Accuracy:", accuracy)
 
-# Display classification report
-print(classification_report(y_test, y_pred, target_names=iris.target_names))
+
+print("report",classification_report(y_te, y_pred, target_names=iris.target_names))
